41d3cf6242725877fa2b904ade0671805b30a025
thebigg <yunior.eury@gmail.com>
2021-04-16 19:16:58 -0500

    -Saving these changes so I don't lose them

--- GoMan.gd
+++ GoMan.gd
@@ -4,22 +4,22 @@ var current_direction = Vector2()
 var can_i_move = true
 
 func _physics_process(delta):
-	if can_i_move == true:
-		if Input.is_action_pressed("ui_right"):
-			current_direction.x = 1
-			current_direction.y = 0
-		
-		if Input.is_action_pressed("ui_left"):
-			current_direction.x = -1
-			current_direction.y = 0
-		if Input.is_action_pressed("ui_down"):
-			current_direction.y = 1
-			current_direction.x = 0
-		
-		if Input.is_action_pressed("ui_up"):
-			current_direction.y = -1
-			current_direction.x = 0
-		
+	if Input.is_action_pressed("ui_right"):
+		current_direction.x = 1
+		current_direction.y = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		current_direction.x = -1
+		current_direction.y = 0
+	if Input.is_action_pressed("ui_down"):
+		current_direction.y = 1
+		current_direction.x = 0
+	
+	if Input.is_action_pressed("ui_up"):
+		print("ui_up")
+		current_direction.y = -1
+		current_direction.x = 0
+	
 	
 	move_and_collide(current_direction)
 
@@ -30,10 +30,10 @@ func _ready():
 
 func _on_Area2D_body_entered(body):
 	print("_on_Area2D_body_entered")
-	can_i_move = true
+	#can_i_move = true
 
 func _on_Area2D_body_exited(body):
 	print("_on_Area2D_body_exited")
-	can_i_move = false
+	#can_i_move = false
 
 
