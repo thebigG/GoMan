8708f13710a8a7b473dfb665eb229fb84279808c
thebigg <yunior.eury@gmail.com>
2021-05-30 15:33:40 -0500

    -Gameover when player wins

--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ var current_food_count = 0
 
 signal start_game
 
-export(PackedScene) var Level100
+export(PackedScene) var Level1
 var life_count = 3
 
 signal enemy_stop
@@ -14,12 +14,7 @@ var stop_man_origin_count = 0
 export var STOPMAN_COUNT = 3
 
 func lose_1_life():
-	
 	if life_count <= 0:
-		print("Game Over!:" + str(life_count))
-		
-		#TODO: Delete the current level1, otherwise we get infinite worlds.
-		#Wait a minute...It's a feature, NOT a bug!
 		game_over()
 		
 	elif life_count > 0 and !get_tree().get_nodes_in_group("Level1")[0].get_node("StopMen").get("set_to_origin"):
@@ -29,15 +24,12 @@ func lose_1_life():
 		lives.remove_child(lives.get_child(lives.get_child_count()-1))
 		$Music.stop()
 		get_tree().get_nodes_in_group("Menu")[0].get_node("Dead").play()
-	#	reset game
-	#	Perhaps have a timeout here??
 		get_tree().get_nodes_in_group("Menu")[0].emit_signal("enemy_stop")
 		
 		$GoMan.set("position", Vector2($GoManOrigin.get("position")))	
 		$Music.play()
 		life_count = life_count -1
 	
-		
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -48,23 +40,29 @@ func _on_Mouth_area_shape_entered(area_id, area, area_shape, self_shape):
 		area.queue_free()
 		$FoodSound.play()
 		current_food_count += 1
+		print("current food count:" + str(current_food_count))
+		
+		print("total_food_count" + str(total_food_count)) 
 
 		get_tree().get_nodes_in_group("Menu")[0].get_node("Menu/StartMenu/Score").text =  "Food:" + str(current_food_count)
-		if current_food_count == total_food_count:
+		if current_food_count == get_tree().get_nodes_in_group("Menu")[0].get("total_food_count"):
 			$YAY.play()
-	if area.is_in_group("Enemy"):
+			print("YAY!")
+			game_over()
+	elif area.is_in_group("Enemy"):
 		lose_1_life()
 
 
 func start_game():
-	var level_handle = Level100.instance()
+	var level_handle = Level1.instance()
 	get_parent().add_child(level_handle)
 	
 	connect("enemy_stop", level_handle.get_node("StopMen"),  "reset_to_origin")	
 	
 	total_food_count = get_tree().get_nodes_in_group('Food').size()
 	
-	total_food_count = get_tree().get_nodes_in_group('Food').size()
+	
+	print("total_food_count:" + str(total_food_count))
 	$Menu/StartMenu.get_node("Score").text =  "Food:" + str(0)
 	$Menu/StartMenu.get_node("StartButton").set("visible", false)
 	$Menu/StartMenu.get_node("Title").set("visible", false)
@@ -74,11 +72,9 @@ func _on_StartButton_pressed():
 	start_game()
 
 func game_over():
-	print("GameOver1")
 	get_tree().get_nodes_in_group("Menu")[0].get_node("Menu/StartMenu").queue_free()
 	queue_free()
 	get_tree().get_nodes_in_group("Menu")[0].get_node("Menu/GameOverMenu").set("visible", true)
-	print("Game over2")
 
 func restart_game():
 	queue_free()
